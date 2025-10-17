f01505977d12bd09e5f030775f6069fbee68e094
mbc90 <michaelbc71@proton.me>
2025-10-17 01:25:52 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Intro
+A simple shell written in Swift
+
+# TODO
+call programs that are not builtins. need to search path
---
+++ Sources/main.swift
@@ -0,0 +1,61 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+import Foundation
+import Swift
+let fileManager = FileManager.default
+let homeUser = fileManager.homeDirectoryForCurrentUser.path
+print("Welcome to xsh!")
+while true {
+    print(fileManager.currentDirectoryPath + " > ", terminator: "")
+    // read the line
+    let cmd = readLine()
+    let args = cmd!.components(separatedBy: .whitespacesAndNewlines) // unwrap it and split by whitespace and new lines
+    // if args[0] is empty print continure on
+    if args[0].isEmpty {
+       continue
+    }
+    // Start matching built ins
+    switch args[0] {
+    case "pwd":
+        print(fileManager.currentDirectoryPath)
+    case "cd":
+        if args.count > 1 { // user did not give a path
+           fileManager.changeCurrentDirectoryPath(args[1])
+        } else {
+            fileManager.changeCurrentDirectoryPath(homeUser)
+            
+        }
+    case "hostname":
+        print(Host.current().localizedName!)
+
+    case "mkdir":
+        if args[1] == "-p"{ // if user wants to create parent directories 
+            do {
+                try fileManager.createDirectory(at: URL.init(filePath: args[2]) , withIntermediateDirectories: true)
+            } catch {
+                print("Error creating Directory: \(error)")
+            }
+        } else {
+            
+            do {
+                try fileManager.createDirectory(at: URL.init(filePath: args[1]) , withIntermediateDirectories: false)
+            } catch {
+                print("Error creating Directory: \(error)")
+            }
+        }
+    default:
+        let process = Process()
+        process.executableURL = URL.init(filePath: args[0])
+        process.arguments = Array(args[1...])
+        do {
+            try process.run() // start the process
+            process.waitUntilExit() // wait until the process is done
+        } catch {
+            print("Error: \(error)")
+        }
+
+        
+    }
+
+    
+}
