2c269f0cbc38463f54c13a6be1344df9f84bf5ef
mbc90 <michaelbc71@proton.me>
2025-10-19 21:16:45 -0400

    separate out matching to separate file

--- Sources/main.swift
+++ Sources/main.swift
@@ -22,49 +22,7 @@ while true {
        continue
     }
     // Start matching built ins
-    switch args[0] {
-    case "exit":
-        print("Goodbye!")
-        exit(0)
-
-    case "pwd":
-        print(fileManager.currentDirectoryPath)
-
-    case "cd":
-        if args.count > 1 { // user did not give a path
-           fileManager.changeCurrentDirectoryPath(args[1])
-        } else {
-            fileManager.changeCurrentDirectoryPath(homeUser)
-            
-        }
-
-    case "hostname":
-        print(Host.current().localizedName!)
-
-    case "mkdir":
-        if args[1] == "-p"{ // if user wants to create parent directories 
-            do {
-                try fileManager.createDirectory(at: URL.init(filePath: args[2]) , withIntermediateDirectories: true)
-            } catch {
-                print("Error creating Directory: \(error)")
-            }
-        } else {
-            
-            do {
-                try fileManager.createDirectory(at: URL.init(filePath: args[1]) , withIntermediateDirectories: false)
-            } catch {
-                print("Error creating Directory: \(error)")
-            }
-        }
-    default:
-        guard let execPath = getPath(command: args[0]) else {
-            print("\(args[0]): Command Not Found") 
-            break
-        }
-        runInteractiveCommand(execPath: execPath, args: Array(args.dropFirst()))
-        
-    }
-
+    matchProc(args: args, fileManager: fileManager, homeUser: homeUser)
     
 }
 
